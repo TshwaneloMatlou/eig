979173eb5f4b250d478b12078a11fc1fc8b19644
TshwaneloMatlou <tshwanelomatlou55@gmail.com>
2023-11-09 10:31:29 +0200

    P/L calculator

--- app/Components/ProfitLossCalculator.tsx
+++ app/Components/ProfitLossCalculator.tsx
@@ -1,13 +1,31 @@
 'use client'
-import React, { useState } from 'react';
+import React, { useState, useEffect  } from 'react';
 
 const ProfitLossCalculator = () => {
   const [lotSize, setLotSize] = useState('0.01');
-  const [stopLossPoints, setStopLossPoints] = useState('0.00100');
+  const [stopLossPoints, setStopLossPoints] = useState('0.00101');
   const [stopLossMoney, setStopLossMoney] = useState('');
-  const [takeProfitPoints, setTakeProfitPoints] = useState('0.00100');
+  const [takeProfitPoints, setTakeProfitPoints] = useState('0.00101');
   const [takeProfitMoney, setTakeProfitMoney] = useState('');
   const [riskRewardRatio, setRiskRewardRatio] = useState('');
+  const [selectedCurrency, setSelectedCurrency] = useState('EURUSD'); // Add state for selected currency
+
+  useEffect(() => {
+    // Calculate Stop Loss Points and Take Profit Points based on the selected currency
+    let calculatedStopLossPoints: number = parseFloat(stopLossPoints);
+    let calculatedTakeProfitPoints: number = parseFloat(takeProfitPoints);
+
+    if (selectedCurrency === 'USDJPY' || selectedCurrency === 'DXY') {
+      calculatedStopLossPoints = Number(calculatedStopLossPoints.toFixed(3));
+      calculatedTakeProfitPoints = Number(calculatedTakeProfitPoints.toFixed(3));
+    } else {
+      calculatedStopLossPoints = Number(calculatedStopLossPoints.toFixed(5));
+      calculatedTakeProfitPoints = Number(calculatedTakeProfitPoints.toFixed(5));
+    }
+
+    setStopLossPoints(calculatedStopLossPoints.toString());
+    setTakeProfitPoints(calculatedTakeProfitPoints.toString());
+  }, [selectedCurrency]);
 
   const handleCalculate = () => {
     // Parse the input values to numbers
@@ -22,6 +40,18 @@ const ProfitLossCalculator = () => {
     }
   
     // Calculate Stop Loss Money and Take Profit Money
+    let calculatedStopLossPoints: number;
+    let calculatedTakeProfitPoints: number;
+  
+    // Adjust the decimal points based on the selected currency
+    if (selectedCurrency === 'USDJPY' || selectedCurrency === 'DXY') {
+      calculatedStopLossPoints = Number(parsedStopLossPoints.toFixed(3));
+      calculatedTakeProfitPoints = Number(parsedTakeProfitPoints.toFixed(3));
+    } else {
+      calculatedStopLossPoints = Number(parsedStopLossPoints.toFixed(5));
+      calculatedTakeProfitPoints = Number(parsedTakeProfitPoints.toFixed(5));
+    }
+  
     const calculatedStopLossMoney = parseFloat((parsedLotSize * parsedStopLossPoints * 100000).toFixed(2));
     const calculatedTakeProfitMoney = parseFloat((parsedLotSize * parsedTakeProfitPoints * 100000).toFixed(2));
   
@@ -29,16 +59,56 @@ const ProfitLossCalculator = () => {
     const riskRewardRatioValue = (calculatedTakeProfitMoney / calculatedStopLossMoney).toFixed(0);
   
     // Update the state with the calculated values
-    setStopLossMoney(calculatedStopLossMoney.toString()); // Convert to string
-    setTakeProfitMoney(calculatedTakeProfitMoney.toString()); // Convert to string
+    setStopLossPoints(calculatedStopLossPoints.toString());
+    setTakeProfitPoints(calculatedTakeProfitPoints.toString());
+    setStopLossMoney(calculatedStopLossMoney.toString());
+    setTakeProfitMoney(calculatedTakeProfitMoney.toString());
     setRiskRewardRatio(riskRewardRatioValue);
   };
+  
   
 
   return (
     <div className="rounded-lg shadow-lg shadow-green-500 p-14">
       <h2 className="text-xl font-semibold mb-4 text-center ">Calculate Profit & Loss</h2>
       <div className="grid gap-4 grid-cols-2">
+        <div className='bg-black col-span-2 text-white p-5 rounded-2xl'>
+          <h1 className='text-center text-green-500 text-[15pt] font-extrabold mb-3'>Description</h1>
+          <p className='mb-3'>This calculator is designed to calculate the Profit and Loss of a trade based on editing the (Currency Pair, Points or LotSize).</p>
+          <p className='mb-2'>* Example Calculate Loss:  LotSize * Stop Loss Points = Stop Loss Money ($)</p>
+          <p className='mb-2'>* Example Calculate Profit:  LotSize * Stop Profit Points = Stop Profit Money ($)</p>
+          <h1 className='bg-blue-500 text-center font-bold my-3 rounded-lg'>Amount of Decimal Numbers (5) </h1>
+          <p className='text-center mb-3 underline'>USDCAD, USDCHF, EURUSD, GBPUSD, AUDUSD, NZDUSD</p>
+          <p className='mb-2'>* Example1: EURUSD = "1.001001"</p>
+          <p className='mb-2'>* Example2: GBPUSD = "1.001002"</p>
+          
+          <h1 className='bg-blue-500 text-center font-bold my-3 rounded-lg'>Amount of Decimal Numbers (3) </h1>
+          <p className='text-center mb-3 underline'>DXY, USDJPY</p>
+          <p className='mb-2'>* Example1: DXY = "1.001"</p>
+          <p className='mb-2'>* Example2: USDJPY = "1.002"</p>
+        </div>
+        <div className='mx-auto text-center pt-8 px-2'>E.I.G</div>
+        <div className="flex flex-col">
+          <label className="mb-2" htmlFor="currency">
+            Currency:
+          </label>
+          <select
+            className="px-2 py-1 border rounded"
+            id="currency"
+            value={selectedCurrency}
+            onChange={(e) => setSelectedCurrency(e.target.value)}
+          >
+            <option value="DXY">DXY</option>
+            <option value="USDCAD">USDCAD</option>
+            <option value="USDCHF">USDCHF</option>
+            <option value="USDJPY">USDJPY</option>
+            <option value="EURUSD">EURUSD</option>
+            <option value="GBPUSD">GBPUSD</option>
+            <option value="AUDUSD">AUDUSD</option>
+            <option value="NZDUSD">NZDUSD</option>
+            <option value="XAUUSD">XAUUSD</option>
+          </select>
+        </div>
         <div className="flex flex-col">
           <label className="mb-2" htmlFor="lotSize">
             Lot Size:
@@ -56,7 +126,7 @@ const ProfitLossCalculator = () => {
             Risk : Reward
           </label>
           <input
-            className="px-2 py-1 border rounded"
+            className="px-2 py-1 border-2 border-red-500 rounded"
             type="text"
             id="riskRewardRatio"
             value={`1 : ${riskRewardRatio}`} // Display the ratio in the desired format
@@ -71,7 +141,7 @@ const ProfitLossCalculator = () => {
             className="px-2 py-1 border rounded"
             type="text"
             id="stopLossPoints"
-            value= {stopLossPoints}
+            value={stopLossPoints}
             onChange={(e) => setStopLossPoints(e.target.value)}
           />
         </div>
@@ -89,10 +159,10 @@ const ProfitLossCalculator = () => {
         </div>
         <div className="flex flex-col">
           <label className="mb-2" htmlFor="stopLossMoney">
-            Stop Loss Money:
+            Stop Loss Money ($):
           </label>
           <input
-            className="px-2 py-1 border rounded"
+            className="px-2 py-1 border-2 border-red-500 rounded"
             type="text"
             id="stopLossMoney"
             value={stopLossMoney}
@@ -101,17 +171,16 @@ const ProfitLossCalculator = () => {
         </div>
         <div className="flex flex-col">
           <label className="mb-2" htmlFor="takeProfitMoney">
-            Take Profit Money:
+            Take Profit Money ($):
           </label>
           <input
-            className="px-2 py-1 border rounded"
+            className="px-2 py-1 border-2 border-red-500 rounded"
             type="text"
             id="takeProfitMoney"
             value={takeProfitMoney}
             readOnly // Make this field read-only
           />
         </div>
-
       </div>
       <div className="mt-4">
         <button
